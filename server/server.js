38a69485720f39bf28bc6a8d72428bc4f7f90da9
techpowerglobal <tpg.techpowerglobal@gmail.com>
2025-09-24 12:17:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,10 +14,16 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://localhost:3000", // CRA fallback
-    ],
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:3000", // CRA fallback
+  "http://72.60.101.69",   // direct IP access
+  "http://ijaems.in",      // your domain
+  "https://ijaems.in",     // https version (after SSL)
+  "http://www.ijaems.in",  // www version
+  "https://www.ijaems.in", // www with https
+],
+
     credentials: true,
   })
 );
